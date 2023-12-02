0778b88032f78527dfa4f186622f5cc3b625d003
Inter <ninjasword1999@gmail.com>
2023-12-02 12:03:51 -0500

    Updated data struct

--- main.cpp
+++ main.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 #include <fstream>
 #include "dataConverter.h"
-#include <vector>
 
 
 int main()
 {
     // gives weather json file name to convert it into a map
     dataConverter obj("weather.json");
-    //
-    //gets the map representation of it, time (string type) is the key and  (float) is the corresponding position in the vector
-    std::unordered_map<string,int> weatherMap = obj.getTimeMap();
 
-    std::vector<std::pair<float,string>> weatherVec = obj.getWeatherVec();
+    //gets the map representation of it, time (string type) is the key and temperature (float) is the value
+    std::unordered_map<string,float> weatherMap = obj.getTimeMap();
 
-    for (auto& pair :weatherVec){
-        std::cout << pair.first << " : " << pair.second << std::endl;
+    // outputs times and temperature. Note that because map is unordered it wont order by time
+    for(auto iter = weatherMap.begin(); iter != weatherMap.end(); iter++){
+        std::cout << "Time: "<<iter->first << " Temp (C): " << iter->second << std::endl;
     }
-
     return 0;
 }
